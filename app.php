84cb74d4c394032fc316ea6610393f3dc1b15c79
basavaraj-nanjappa <127355458+basavaraj-nanjappa@users.noreply.github.com>
2024-12-30 14:20:53 +0530

    created integration branch commit node

--- app.php
+++ app.php
@@ -3,14 +3,22 @@ $version = "1.0";
 ?>
 
 <html>
-    <head>
-        <title>App</title>
-    </head>
-    <body>
-        <h1>Application is running</h1>
-        <h2>This is version <?php echo $version?></h2>
-        <ol>
-            <li>initial commit</li>
-        </ol>
-    </body>
+
+<head>
+    <title>App</title>
+</head>
+
+<body>
+    <h1>Application is running</h1>
+    <h2>This is version <?php echo $version ?></h2>
+    <ol>
+        <li>initial commit</li>
+        <li>
+            Branch integration-rel-r01
+            <ul>
+            </ul>
+        </li>
+    </ol>
+</body>
+
 </html>
